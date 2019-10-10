3bd2981d8f9eef197740d2401811811cddd25da8
Alberqing <zqysly@qq.com>
2019-10-10 17:04:43 +0800

    1.完成webpack基础配置 2.制作启动页面

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const dev = require('./webpack.dev');
 const prod = require('./webpack.prod');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -15,19 +16,54 @@ const base = {
             use: 'babel-loader'
         }, {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [
+                !isDev && MiniCssExtractPlugin.loader,       // 样式抽离
+                isDev && 'style-loader', 
+                {
+                    loader:'css-loader',
+                    options: {
+                        importLoaders: 1                     // 引入的文件调用后面的loader处理
+                    }
+                },
+                "postcss-loader",                            // 样式前缀
+            ].filter(Boolean)
+        }, {
+            test: /\.scss$/,
+            use: [
+                !isDev && MiniCssExtractPlugin.loader,
+                isDev && 'style-loader',
+                "css-loader",
+                "sass-loader"
+            ].filter(Boolean)
+        }, {
+            test: /\.less$/,
+            use: "less-loader",
+        }, {
+            test: /\.stylus$/,
+            use: "stylus-loader",
+        }, {
+            test: /\.(jpe?g|png|svg|gif)$/,
+            use: {
+                loader: "file-loader",
+            }
+        }, {
+            test: /\.(woff|ttf|eot|otf)$/,
+            use: "file-loader"
         }]
     },
     output: {
-        filename: 'js/[name].bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, '../dist')
     },
     resolve: {  // 引入js、jsx文件时，无需添加后缀
         extensions: ['.js', '.jsx'],        
     },
     plugins: [
+        !isDev && new MiniCssExtractPlugin({
+            filename: 'css/[name].[contentHash].css'
+        }),
         new HtmlWebpackPlugin({
-            filename: 'html/index.html',
+            filename: 'index.html',
             template: path.resolve(__dirname, '../public/index.html'),
             hash: true,
             minify: !isDev && {
@@ -35,7 +71,7 @@ const base = {
                 collapseWhitespace: true, // 将html文件折叠成一行
             }
         })
-    ]
+    ].filter(Boolean)
 }
 
 module.exports = () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css'
 
 export default class App extends Component {
     render() {
         return (
-            <div>hello world!</div>
+            <div className="app">
+                <img src={logo} alt="图片" className="logo"/>
+                <a className="text" href="https://juejin.im/user/5d38178cf265da1bb27773f4/posts" target="_blank">
+                    欢迎进入Number的掘金主页
+                </a>      
+            </div>
         )
     }
 }
