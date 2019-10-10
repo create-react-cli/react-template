ca23207f5cab238af4b66d26f3b5683e70960089
Alberqing <zqysly@qq.com>
2019-10-10 17:07:31 +0800

    添加文字

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,13 @@ export default class App extends Component {
             <div className="app">
                 <img src={logo} alt="图片" className="logo"/>
                 <a className="text" href="https://juejin.im/user/5d38178cf265da1bb27773f4/posts" target="_blank">
-                    欢迎进入Number的掘金主页
+                    点击进入我的掘金主页
+                </a>  
+                <a className="text" href="https://github.com/Alberqing" target="_blank">
+                    点击进入我的github主页
+                </a>  
+                <a className="text" href="https://blog.csdn.net/Alberqing_" target="_blank">
+                    点击进入我的CSDN主页
                 </a>      
             </div>
         )
