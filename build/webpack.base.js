da956b309756354133459870ae9f05eb1aecb320
Alberqing <zqysly@qq.com>
2019-10-11 09:18:40 +0800

    增加开发环境打包命令

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -90,7 +90,14 @@ const base = {
             parallel: true
         }),
         new webpack.HotModuleReplacementPlugin(),
-    ].filter(Boolean)
+    ].filter(Boolean),
+    devServer: {
+        hot:true,                                        // 热更新
+        port: 3000,                                      // 端口号
+        compress: true,                                  // 提升页面返回速度
+        open: true,                                      // 启动服务后自动启动浏览器
+        contentBase: path.resolve(__dirname, '../dist'), // webpack启动服务会在dist目录下
+    }
 }
 
 module.exports = () => {
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,12 +1,4 @@
-const path = require('path');
 
 module.exports = {
     mode: 'development',
-    devServer: {
-        hot:true,                                        // 热更新
-        port: 3000,                                      // 端口号
-        compress: true,                                  // 提升页面返回速度
-        open: true,                                      // 启动服务后自动启动浏览器
-        contentBase: path.resolve(__dirname, '../dist'), // webpack启动服务会在dist目录下
-    }
 }
