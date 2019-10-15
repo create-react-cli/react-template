7af92bba919af6e22ca05d1933a4003e3b81bf6e
Alberqing <zqysly@qq.com>
2019-10-15 20:40:43 +0800

    压缩css文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,8 +1,16 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const optimizeCssAssetsWebpackPlugin= require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin= require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'production',
+    optimization: {                                        // 压缩方案
+        minimizer: [
+            new TerserWebpackPlugin(),
+            new optimizeCssAssetsWebpackPlugin(),
+        ]
+    },
     plugins: [
         new CleanWebpackPlugin(),                         // 打包前清空dist目录
         new UglifyJsPlugin({                              // 打包后自动去除debugger、console等        
